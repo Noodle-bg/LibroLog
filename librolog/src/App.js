eb5698d4367641fd1a4298e967d09e24ddfc6614
BG Pranav <142234652+Noodle-bg@users.noreply.github.com>
2023-10-19 10:47:27 +0530

    header

--- librolog/src/App.js
+++ librolog/src/App.js
@@ -4,19 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header  >
+        <nav className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ul className="nav-items">
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Signup</a></li>
+        </ul>
+        </nav>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <code></code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
