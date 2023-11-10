f734408d00160bfa99f0555e27d88d6457456441
BG Pranav <142234652+Noodle-bg@users.noreply.github.com>
2023-11-10 15:12:23 +0530

    forgot to put cat display istelf

--- librolog/src/Categorydisplay.js
+++ librolog/src/Categorydisplay.js
@@ -1,28 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import Searchabledropdown from "./Searchabledropdown";
 import logo from './logo.svg';
-const Categorydisplay =()=>{
-    return(
-        <div>
-        <header className="header">
+import "./App.css";
+import Menubar from "./Menubar";
+import { usePage } from './PageContext';
+
+const Categorydisplay = () => {
+  const [bestsellers, setBooks] = useState([]);
+  const { currentPage } = usePage();
+
+  useEffect(() => {
+    const fetchBestsellers = async () => {
+      try {
+        const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:${currentPage}&maxResults=30&fields=items(volumeInfo(title,authors,imageLinks,industryIdentifiers))`);
+        if (res.data.items && res.data.items.length > 0) {
+          setBooks(res.data.items);
+          console.log(res.data);
+        } else {
+          console.log("Data is missing or empty.");
+        }
+      } catch (error) {
+        console.error("Error fetching bestsellers:", error);
+      }
+    };
+    fetchBestsellers();
+  }, [currentPage]);
+
+  return (
+    <div>
+      <header className="header">
         <nav className="App-header">
           <div className='logo-background'>
-          <Link to='/' className="logo-link">
-            <img src={logo} className="App-logo" alt="logo" height="100px" />
-          </Link>
+            <Link to='/' className="logo-link">
+              <img src={logo} className="App-logo" alt="logo" height="100px" />
+            </Link>
           </div>
-          <Searchabledropdown/>
+          <Searchabledropdown />
           <ul className="nav-items">
             <li><Link>Login</Link></li>
             <li><Link>Sign-up</Link></li>
             <li><Link to="/dashboard">Dashboard</Link></li>
           </ul>
         </nav>
-        
       </header>
+      <Menubar/>
 
-        </div>
-    )
-}
+      <div className="book-grid">
+        {bestsellers.map((book,index) => (
+                  <div className="card-container-cat" key={index}>
+                  <Link>
+                  <div className="card-cat" style={{ backgroundImage: `url(${book.volumeInfo.imageLinks.thumbnail})` }}></div></Link>
+                  <div className="card-title-cat">{book.volumeInfo.title}</div>
+        
+                </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Categorydisplay;
